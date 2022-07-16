27f8c4ee1dd679ab99affe4be61f161cd7687761
Tim Traylor <ttraylor83@gmail.com>
2022-07-15 21:23:16 -0700

    question 2 updated

--- js/app.js
+++ js/app.js
@@ -27,7 +27,9 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function multiply(a, b) { //eslint-disable-line
-
+  let myMulti = a * b ;
+  let myString = `The product of ${a} and ${b} is ${myMulti}.`;
+  return [myMulti, myString];
 }
 
 // Here is the test for multiply(); uncomment it to run it
